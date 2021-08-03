4de808de425903cb2834b3455b2ef1dcbdcbf1c8
Lucas de Souza <lucasdsm.personal@gmail.com>
2021-08-03 11:44:01 -0300

    Movimentação Damakos adicionada

---
+++ GoodEvil-ProjetoIntegrado/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    public int x;
+    public int y;
+    public Rigidbody2D rb;
+    public Animator animator;
+    public Vector2 movement;
+       
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+}
