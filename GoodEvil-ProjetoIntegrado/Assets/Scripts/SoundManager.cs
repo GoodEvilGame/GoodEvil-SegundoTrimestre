7cdc8411be56f1f8fe2bf05d6a246e090fd18e6c
Vinícius Silva <vivisec007@gmail.com>
2021-08-11 11:47:18 -0300

    Adição e configuração dos sons do menu

---
+++ GoodEvil-ProjetoIntegrado/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class Bullet : MonoBehaviour
+{   
+    public AudioClip vandal;
+    AudioSource audioSource;
+    public float speed = 40f;
+    public Rigidbody2D rb;
+    public string TagE;
+    public float Direction;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.Play();
+        rb.velocity =  new Vector2(Direction * speed, 0);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag($"{TagE}") || collision.gameObject.CompareTag("plataform"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+}
